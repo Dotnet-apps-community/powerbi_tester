963b7b3a22a0210d984ba2042a3d0dc723e1d70f
xRenatoCodex <renatozapata199788@gmail.com>
2025-08-02 10:41:34 -0500

    SqlConnection class created

---
+++ Analizer/Tables/BigQueryTable.cs
@@ -0,0 +1,20 @@
+﻿using Analizer.Tables.Enums;
+
+namespace Analizer.Tables;
+
+public class BigQueryTable:Table
+{
+    public Connectors Connector = Connectors.BigQuery;
+    public SqlConnection? SqlConnection { get; set; }
+}
+
+public class SqlConnection
+{
+    public string Query { get; set; } = "";
+    public bool IsAllColumnsSelected { get; set; } = false;
+    public bool HasStartRangeParameter { get; set; }
+    public bool HasEndRangeParameter { get; set; }
+    public int RowsQuantityExtracted { get; set; }
+    public string[] ColumnsNameJoined { get; set; } = [];
+    public int QueryWeightMb { get; set; }
+}
